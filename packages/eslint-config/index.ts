f16638246bef3179b742c2f2e59ec6ddf43beab3
joseph Estevez <joseph.estevez@protonmail.com>
2022-09-26 21:48:13 -0400

    Initial commit
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⡿⣿⣻⡿⣿⡿⣿⣟⢿⡛⡗⡿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡿⣽⢾⢽⣳⢯⢿⢽⢯⢷⣳⣟⡷⡛⢕⢎⡪⡪⢎⢯⢿⣿⣿⣿⣿
    ⣿⣿⣿⣿⢿⢿⡿⣿⣻⣟⣯⢯⢿⢽⡽⡯⣯⢿⢽⢯⢿⣽⣳⣗⣟⡷⣝⢜⡘⣜⢜⢼⢽⣿⣿⣿⣿
    ⣿⣿⡓⡯⡍⡇⡏⠮⣸⢨⢪⡿⡽⣯⢯⡿⣽⡽⣯⡿⣟⡾⣞⡷⡯⣯⢯⡷⡩⡒⡝⣮⣿⣿⣿⣿⣿
    ⣿⣿⣞⢜⢌⢖⢜⢝⢜⢼⢽⣞⣯⢯⡯⣯⢷⣻⣳⣟⣽⢽⢯⢯⢿⢽⢽⣽⣳⢧⣳⣷⣿⣿⣿⣿⣿
    ⣿⣿⣷⡝⡎⡎⣕⢅⢧⢿⡽⣞⡾⢯⣟⣽⢽⡳⡏⡾⡱⢷⣧⡛⣬⣭⣷⣵⣫⢟⣞⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⢵⡹⡢⣃⣯⢯⡯⣷⢋⣟⠮⣞⣏⣪⣗⢕⢱⣫⣾⣇⠈⣿⣿⣿⣿⣿⢵⡫⣟⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⡽⣞⣦⢯⡯⣯⢣⣪⡆⠙⣿⣿⣿⣿⣷⣟⣿⣿⣿⡄⢸⣿⣿⣿⡿⡙⢷⢮⣎⣻⣿⣿
    ⣿⣿⣿⣿⡿⡽⡋⡢⡿⣽⢣⢱⣿⣷⠀⠘⣿⣿⣿⣿⡿⣾⣟⡿⣿⣼⢿⣟⣿⠰⡑⡍⢯⢿⣿⣿⣿
    ⣿⣿⣿⣿⣹⣼⡧⡱⡿⡝⢔⢹⣿⣿⣧⡀⣿⣿⡿⣻⣾⣿⣽⣟⣿⣺⣿⣽⡏⢎⣬⡊⡆⡻⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣇⣾⣯⣮⣼⣐⢍⢻⢻⣷⣽⣷⣟⢿⣻⣾⣺⣽⣯⣾⢿⢞⢜⠔⢼⣿⣷⣮⣿⣿⣿
    ⣿⣿⣿⣿⣿⢿⣽⡷⣿⢾⣻⣿⢷⡡⡣⢫⢳⢿⣽⡿⣷⣟⣯⣿⡾⣻⡑⢽⣷⣧⣹⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣟⣿⣽⢿⣻⣿⣻⣽⣿⡇⢎⣮⡢⡣⡩⡝⣝⣭⣹⣲⣮⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣚⣿⣽⣿⣻⣽⣿⣽⡾⣝⢶⣧⣻⣿⣿⣷⣿⣿⣟⣯⣿⣿⣽⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣷⣎⡕⡟⢯⢻⢺⢓⢝⢝⢼⣿⣷⣿⡷⣿⣿⣿⣿⣿⣿⣿⣷⡹⡝⣟⣿⣾⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣾⣾⣾⣾⣗⢵⢱⢿⢿⢟⢯⣚⣿⣿⣿⣿⣿⣿⣿⣿⢪⣾⢿⣿⢯⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣷⣿⣿⡪⡲⡵⣿⣿⣿⣿⣿⣿⣿⡽⣟⣿⣷⣻⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠫⣇⢯⣻⣿⣿⣿⣿⣿⣿⠛⣿⣯⢿⣻⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡻⡐⠅⠀⠄⠄⢄⠡⢉⠛⡛⢋⢐⢈⢿⣾⢿⣻⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢽⢏⢇⣮⠀⠁⠌⠌⡐⠨⢐⠐⡠⢁⠢⢐⢨⣶⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢯⢫⣾⣿⡇⠀⠨⠨⢐⠠⢑⠐⠨⢐⢐⠨⢐⠸⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⢷⢯⣳⣿⣿⡟⠀⢈⠨⠠⠡⠨⢐⠨⠨⢐⠠⠨⢐⠐⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣻⡽⣽⣿⣿⣿⠃⠠⠐⠨⢐⢁⢊⢐⠨⢐⢐⠨⢈⢐⢈⢻⣿⣿⣿⣿⣿⣿⣿
    ⣿⣯⡿⣿⣿⣿⣿⡽⡗⣽⣿⣿⣿⡟⠀⠠⠨⠨⢐⠠⠂⠔⠨⢐⠠⠨⢐⠐⠠⢺⣿⣿⣿⣿⣿⣿⣿
    ⣧⢓⢿⣝⣟⡯⡾⣛⣼⣿⣿⣿⣿⣧⡔⠀⠌⡐⡐⠨⢈⠌⡨⢐⠨⢈⢐⠨⢨⣶⣿⣿⣿⣿⣿⣿⣿
    ⣿⣧⣇⣝⣚⣝⣼⣿⣿⣿⣿⣿⣿⣿⠀⠠⢀⣷⣶⣧⣦⣂⣂⣂⣬⡤⠀⡂⢼⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠅⢸⣿⣿⣿⣿⣿⣿⣿⣿⡁⢐⠠⢈⠙⠻⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢁⠨⠐⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠂⠨⠐⡈⢙⢿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⠂⠄⣡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠂⠐⡀⠢⣍⠻
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠃⡐⠠⢈⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⡁⠄⣂
    ⣿⣿⣿⣿⣿⣿⣿⠋⠄⢌⠔⡁⢌⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣴⣈⣀⣁⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿

---
+++ packages/eslint-config/index.ts
@@ -0,0 +1,23 @@
+import type { Linter } from "eslint";
+
+const config: Linter.Config = {
+  env: {
+    es2022: true,
+    browser: true,
+  },
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: { version: "detect" },
+  },
+  ignorePatterns: ["dist", "build", "node_modules"],
+};
+
+export = config;
